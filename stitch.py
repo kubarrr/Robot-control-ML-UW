98060dab0cbaac3c9301499fb19c8cdc77a3c98d
Kuba <01171262@pw.edu.pl>
2024-11-25 21:34:59 +0100

    Task 5

--- stitch.py
+++ stitch.py
@@ -155,7 +155,7 @@ def apply_projective_transformation(src_image, H):
     cv2.imshow("orginal", src_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    cv2.imshow("transforem", dst_image)
+    cv2.imshow("transformed", dst_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     return dst_image, min_x, max_x, min_y, max_y
@@ -274,8 +274,175 @@ H=np.array([[ 7.98740818e-01, -2.10795707e-02,  1.69309897e+02],
        [-1.33520799e-04, -3.90246481e-05,  1.00000000e+00]])
 # apply_projective_transformation(src_img, H)
 
+
+############################
+########## TASK 5 ########## 
+############################
+
+src_img = cv2.imread(f"./stitching/img1.png")
+# cv2.imshow("example source image", src_img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+dst_img = cv2.imread(f"./stitching/img2.png")
+# cv2.imshow("example destination image", dst_img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+def get_new_shape_dst(dst_img, src_img, offset_x, offset_y):
+    height_src, width_src= src_img.shape[0], src_img.shape[1]
+    height_dst, width_dst= dst_img.shape[0], dst_img.shape[1]
+
+    offset_x=offset_x
+    offset_y=offset_y
+   
+    size_x=width_src
+    size_y=height_src
+    new_shape = np.zeros((size_y, size_x, 3), dtype=np.uint8)
+    # print(new_shape.shape)
+    # print(src_img.shape)
+    if offset_x>0:
+        new_shape[-offset_y:height_dst-offset_y, :width_dst, :] = dst_img[:, :, :]
+    return new_shape
+
+src_img_transformed, offset_x, max_x, offset_y, max_y=apply_projective_transformation(src_img, H)
+dst_reshaped=get_new_shape_dst(dst_img, src_img_transformed, offset_x, offset_y)
+cv2.imshow("dst reshaped image", dst_reshaped)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imshow("src transformed image", src_img_transformed)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+def calc_cost(pixel1, pixel2):
+    diff = np.abs(pixel1 - pixel2)
+    grayscale = 0.3 * diff[0] + 0.59 * diff[1] + 0.11 * diff[2]
+    return grayscale ** 2
+
+def find_path(src_img_transformed, dst_img, offset_x, offset_y, shape_y):
+    height_dst, width_dst= dst_img.shape[0], dst_img.shape[1]
+    height_src, width_src= src_img_transformed.shape[0], src_img_transformed.shape[1]
+    if offset_x>0:
+        src_img_overlap=src_img_transformed[-offset_y:-offset_y+shape_y, :width_dst-offset_x]
+        dst_img_overlap=dst_img[-offset_y:-offset_y+shape_y, offset_x:]
+        row_length=width_dst-offset_x
+    else:
+        src_img_overlap=src_img_transformed[-offset_y:-offset_y+shape_y, -offset_x:width_dst]
+        dst_img_overlap=dst_img[-offset_y:-offset_y+shape_y, :width_dst+offset_x]
+        row_length=width_dst+offset_x
+    
+    col_length=shape_y
     
 
+    cv2.imshow("src image", src_img_overlap)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    cv2.imshow("dst image", dst_img_overlap)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    #x_min, y_min, x_max, y_max = 169, 0, 1279, 719
+    #col_length=height_src
+    cost_tab=np.zeros((col_length, row_length))
+    path_ids=np.zeros((col_length, row_length))
+    for i in range(row_length):
+        cost_tab[0, i]=calc_cost(src_img_overlap[0, i], dst_img_overlap[0, i])
+    for i in range(1, col_length):
+        for j in range(row_length):
+            best_id=0
+            best_cost=1000000
+            for k in range(-1, 2, 1):
+                if 0<=j+k<row_length:
+                    prev_cost=cost_tab[i-1, j+k]
+                    cost=calc_cost(src_img_overlap[i, j], dst_img_overlap[i, j])
+                    cost_sum=prev_cost+cost
+                    if cost_sum<best_cost:
+                        best_id=k
+                        best_cost=cost_sum
+            cost_tab[i, j]=best_cost
+            path_ids[i, j]=best_id
+
+    best_cost=1000000
+    best_id=0
+    best_path=[]
+    for i in range(row_length):
+        if cost_tab[col_length-1, i]<best_cost:
+            best_cost=cost_tab[col_length-1, i]
+            best_id=i
+    best_path.append([col_length-1, best_id])
+    for i in range(col_length-2, -1, -1):
+        x=int(best_path[-1][1])
+        best_id=path_ids[i+1, x]
+        best_path.append([i, int(x+best_id)])
+    
+    id_high=best_path[-1][1]
+    for i in range(height_dst+offset_y-shape_y):
+        best_path.append([-i, id_high])
+
+    id_low=best_path[0][1]
+    for i in range(-offset_y):
+        best_path.insert(0, [shape_y-1+i, id_low])
+    
+    for i in range(len(best_path)):
+        best_path[i][0]=best_path[i][0]+height_dst+offset_y-shape_y
+    return best_path
+
+print(offset_x, offset_y, size[1])
+best_seam=find_path(src_img_transformed, dst_reshaped, offset_x, offset_y, size[1])
+print(best_seam)
+
+def plot_seam(image1, image2, seam, offset_x):
+    if offset_x>0:
+        offset_x1=0
+        offset_x2=offset_x
+    else:
+        offset_x1=-offset_x
+        offset_x2=0
+    plt.figure(figsize=(12, 6))
+    
+    plt.subplot(1, 2, 1)
+    plt.imshow(image1)
+    plt.plot([s[1]+offset_x1 for s in seam], [s[0] for s in seam], color='red', linewidth=1)
+    plt.title("source image")
+    
+    plt.subplot(1, 2, 2)
+    plt.imshow(image2)
+    plt.plot([s[1]+offset_x2 for s in seam], [s[0] for s in seam], color='red', linewidth=1)
+    plt.title("destination image")
+    
+    plt.show()
+
+plot_seam(src_img_transformed, dst_reshaped, best_seam, offset_x)
+
+def stitch_images(dst_reshaped, src_img_transformed, seam, offset_x):
+    #height, width= src_img_transformed.shape[0], src_img_transformed.shape[1]
+    #height, width= dst_reshaped.shape[0], dst_reshaped.shape[1]
+    if offset_x>0:
+        height, width= src_img_transformed.shape[0], src_img_transformed.shape[1]
+        new_image=np.zeros((height, width+offset_x, 3), dtype=np.uint8)
+        for i in range(height):
+            new_image[i, :(seam[height-1-i][1]+offset_x)]=dst_reshaped[i, :(seam[height-1-i][1]+offset_x)]
+            new_image[i, (seam[height-1-i][1]+offset_x):]=src_img_transformed[i, (seam[height-1-i][1]):]
+    else:
+        height, width= dst_reshaped.shape[0], dst_reshaped.shape[1]
+        new_image=np.zeros((height, width-offset_x, 3), dtype=np.uint8)
+        print(new_image.shape)
+        for i in range(height):
+            new_image[i, (seam[height-1-i][1]-offset_x):]=dst_reshaped[i, (seam[height-1-i][1]):]
+            new_image[i, :(seam[height-1-i][1]-offset_x)]=src_img_transformed[i, :(seam[height-1-i][1]-offset_x)]
+            #new_image[i, :(seam[height-1-i][1]-offset_x)]=src_img[i, :(seam[height-1-i][1]-offset_x)]
+            # print(seam[i][1]-offset_x)
+            # print((new_image[i, int(seam[i][1]-offset_x):]).shape)
+            # print((dst_img[i, seam[i][1]:]).shape)
+            #new_image[i, (seam[height-1-i][1]-offset_x):]=dst_img[i, (seam[height-1-i][1]):]
+    return new_image
+
+stitched_img=stitch_images(dst_reshaped, src_img_transformed, best_seam, offset_x)
+cv2.imshow("stitched image", stitched_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite("task_5_stitched.jpg", stitched_img)
 
 
 
